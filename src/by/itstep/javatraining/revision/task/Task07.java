33e7f823d8e9edc307690a260fb0649fae4a37cf
Egor <gerritmix745@gmail.com>
2024-04-01 14:42:09 +0300

    Initial commit

---
+++ src/by/itstep/javatraining/revision/task/Task07.java
@@ -0,0 +1,52 @@
+package by.itstep.javatraining.revision.task;
+
+/*	Task 07. The sum of the digits of a four-digit number [сумма цифр четырёхзначного числа]
+ *
+ *	Дано целое четырёхзначное число (или меньше). Найдите сумму его цифр.
+ *
+ *  Примечание
+ *  1) Число может быть как положительным, так и отрицательным.
+ *  2) Не забудьте про "защиту от дурака": если число превышает четырёхзначное, то нужно возвратить ноль
+ *  3) Для решения задачи рекомендуется использовать только встроенные в язык Java операции.
+ *
+ *	Формат входных данных [input]
+ *	Метод принимает на вход любое целое число в диапазоне типа int.
+ *
+ *	Формат выходных данных [output]
+ *	Метод должен возвратить одно целое число - ответ на задачу или ноль - в случае неверных данных.
+ *
+ *	[Sample method input  1]: 1234
+ *	[Sample method output 1]: 10
+ *
+ *	[ input 2]: -9999
+ *	[output 2]: 36
+ *
+ *	[ input 3]: 0
+ *	[output 3]: 0
+ *
+ * 	[ input 4]: 12345
+ *	[output 4]: 0
+ *
+ * 	[ input 5]: -12345
+ *	[output 5]: 0
+ *
+ * 	[ input 6]: -123
+ *	[output 6]: 6
+ *
+ * 	[ input 7]: 12
+ *	[output 7]: 3
+ */
+
+public class Task07 {
+    public static int start(int number) {
+        if (number > 9999 || number < -9999) {
+            return 0;
+        }
+        number = number > 0 ? number : -number;
+        int sum = number % 10;
+        sum += (number / 10) % 10;
+        sum += (number / 100) % 10;
+        return sum + number / 1000;
+
+    }
+}
